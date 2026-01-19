f7f731e6acd3d1ac4c600e41b9e60d091507329c
TheUnseenBug <dennis.granheimer@live.se>
2026-01-19 15:54:19 +0100

    Initial Commit

---
+++ TrueFlight Watch App/TrueFlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrueFlightApp.swift
+//  TrueFlight Watch App
+//
+//  Created by Dennis Granheimer on 2026-01-19.
+//
+
+import SwiftUI
+
+@main
+struct TrueFlight_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrueFlight/TrueFlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrueFlightApp.swift
+//  TrueFlight
+//
+//  Created by Dennis Granheimer on 2026-01-19.
+//
+
+import SwiftUI
+
+@main
+struct TrueFlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
